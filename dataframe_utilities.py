68d4508a9d345a230d1a009029fe61c909b4c929
Softypo <ggrsoft@gmail.com>
2021-06-03 00:28:40 -0600

    hotfix

--- dataframe_utilities.py
+++ dataframe_utilities.py
@@ -3,13 +3,13 @@ import pandas as pd
 from os import listdir
 
 # load csv and excel files from a folder and store them in a dictionary
-def loader(path=None, ext=False):
+def loader(path=None, index_col=None, ext=False):
     """load csv and excel files from a folder and store them in a dictionary.
     
     parameters
     ----------
-    path : folder path
-    folder containg the desired csv and/or excel files.
+    path : folder path containg the desired csv and/or excel files.
+    index_col : int, column number to be used as index.
     """
     try:
         files = {}
@@ -17,9 +17,9 @@ def loader(path=None, ext=False):
             if ext==False: name = filename.rpartition('.')[0].replace(' ', '_')
             else: name =filename.replace(' ', '_')
             if filename.endswith('.xlsx'):
-                files[name] = pd.read_excel(path+'\\'+filename)
+                files[name] = pd.read_excel(path+'\\'+filename, index_col=index_col)
             elif filename.endswith('.csv'):
-                files[name] = pd.read_csv(path+'\\'+filename)
+                files[name] = pd.read_csv(path+'\\'+filename, index_col=index_col)
         return files
     except Exception:
         print ('Exception: No files loaded for \\', path)
@@ -61,7 +61,7 @@ def drop_nan (df=None, axis=1, how='all', inplace=False):
     ----------
     df : pandas dataframe or mutable array like containing them.
     axis : 0 for rows wise or 1 for columns wise.
-    how : {'any', 'all'}, default 'any'
+    how : {'any', 'all'}, default 'any'.
     * 'any' : If any NA values are present, drop that row or column.
     * 'all' : If all values are NA, drop that row or column.
     inplace : bool, default False, if True, do operation inplace and return None.
@@ -97,22 +97,29 @@ def getDuplicateColumns(df):
     """
     # Create an empty set
     duplicateColumnNames = set()
-      
+
+    # comparing columns agains index
+    for x in range(df.shape[1]):
+        col = df.iloc[:, x]
+        tempdf = df.set_index(col)
+        if tempdf.index.equals(df.index):
+            duplicateColumnNames.add(df.columns.values[x])
+
     # Iterate through all the columns 
     # of dataframe
     for x in range(df.shape[1]):
-          
+
         # Take column at xth index.
         col = df.iloc[:, x]
-          
+
         # Iterate through all the columns in
         # DataFrame from (x + 1)th index to
         # last index
         for y in range(x + 1, df.shape[1]):
-              
+
             # Take column at yth index.
             otherCol = df.iloc[:, y]
-              
+
             # Check if two columns at x & y
             # index are equal or not,
             # if equal then adding 
@@ -132,6 +139,8 @@ def dropEqualColumns (df=None, inplace=False):
     ----------
     df : pandas dataframe or mutable array like containing them.
     inplace : bool, default False, if True, do operation inplace and return None.
+
+    Note: returns none for array like inputs.
     """
     try:
         if isinstance(df, pd.DataFrame):
@@ -144,19 +153,47 @@ def dropEqualColumns (df=None, inplace=False):
                 dupes = getDuplicateColumns(d)
                 print(dupes)
                 lst.append(d.drop(columns = dupes, inplace=inplace))
-            if inplace==False: return lst
         elif isinstance(df, (dict)):
             lst={}
             for k, d in df.items():
                 dupes = getDuplicateColumns(d)
-                print(dupes)
+                print('duplicates in ',k,dupes)
                 lst[k] = d.drop(columns = dupes, inplace=inplace)
+    except Exception:
+        print ('Exception: input is not a pandas dataframe nor a list/tuple containing them')
+
+
+
+# rename index title name
+def renameIndex (name, df=None, inplace=False):
+    """rename index column title.
+    
+    parameters
+    ----------
+    name: string, new name
+    df : pandas dataframe or mutable array like containing them.
+    inplace : bool, default False, if True, do operation inplace and return None.
+
+    Note: returns none for array like inputs.
+    """
+    try:
+        if isinstance(df, pd.DataFrame):
+            return df.index.set_names(name, inplace=inplace)
+        elif isinstance(df, (list, tuple)):
+            lst=[]
+            for d in df:
+                lst.append(d.index.set_names(name, inplace=inplace))
+            if inplace==False: return lst
+        elif isinstance(df, (dict)):
+            lst={}
+            for k, d in df.items():
+                lst[k] = d.index.set_names(name, inplace=inplace)
             if inplace==False: return lst
     except Exception:
         print ('Exception: input is not a pandas dataframe nor a list/tuple containing them')
 
+
 def main ():
-    target_folder = loader('Data for Datathon')
     pass
 
 if __name__ == "__main__":
